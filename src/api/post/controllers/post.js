5f641c50cbb31fe11598d994201d51958026c7a0
luannt <luannt2@sendo.vn>
2023-11-18 14:31:21 +0700

    feat: init cms

---
+++ src/api/post/controllers/post.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * post controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+    console.log("query: ", query)
+
+    const posts = await strapi.entityService.findMany("api::post.post", query);
+
+    // const sanitizedEntity = await this.sanitizeOutput(posts);
+
+    return this.transformResponse(posts[0]);
+  },
+}));
---
+++ src/api/post/routes/post.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * post router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+const defaultRouter = createCoreRouter("api::post.post");
+
+// function to add to or override default router methods
+const customRouter = (innerRouter, routeOverride = [], extraRoutes = []) => {
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes;
+
+      const newRoutes = routes.map((route) => {
+        let found = false;
+
+        routeOverride.forEach((override) => {
+          if (
+            route.handler === override.handler &&
+            route.method === override.method
+          ) {
+            found = override;
+          }
+        });
+
+        return found || route;
+      });
+
+      return newRoutes.concat(extraRoutes);
+    },
+  };
+};
+
+// Overide the default router with the custom router to use slug.
+const myOverrideRoutes = [
+  {
+    method: "GET",
+    path: "/posts/:slug",
+    handler: "api::post.post.findOne",
+  },
+];
+
+const myExtraRoutes = [];
+
+module.exports = customRouter(defaultRouter, myOverrideRoutes, myExtraRoutes);
+
+// module.exports = createCoreRouter('api::post.post');
