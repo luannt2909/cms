8dbcc5203eaee1e6b8f33a329c273a0319d5ec1b
luannt <luannt2@sendo.vn>
2023-11-19 23:56:28 +0700

    refactor: remove unused code

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,16 +1,8 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  // 'strapi::cors',
+  'strapi::cors',
   'strapi::poweredBy',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['*']
-    }
-  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
