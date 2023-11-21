a56f38640895895e459596a27143e2c52dfe6055
luannt <luannt2@sendo.vn>
2023-11-21 15:53:03 +0700

    feat: integrate moesif for monitoring

---
+++ config/plugin.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'strapi-plugin-moesif': {
+    enabled: true,
+    config: {
+      moesif: {
+        //custom config passed to moesif middleware goes here
+      }
+    },
+  },
+}
